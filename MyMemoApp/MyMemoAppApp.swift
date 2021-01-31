086fdd85898049a2f99ad7082b101bc554100230
three523 <71269216+three523@users.noreply.github.com>
2021-01-31 14:54:00 +0900

    Initial Commit

---
+++ MyMemoApp/MyMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemoAppApp.swift
+//  MyMemoApp
+//
+//  Created by apple on 2021/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MyMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
